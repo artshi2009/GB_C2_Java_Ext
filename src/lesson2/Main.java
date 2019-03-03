a3b3bbf0afededd566fbcd3b5e22fc0871745c6f
Xakep <artshi2009@yandex.ru>
2019-03-03 19:15:52 +0300

    add smiles in array :)

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -2,9 +2,9 @@ package lesson2;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] array = new String[][]{{"3"},
-                                            {"7", "9"},
-                                            {":о", "3", ":)"},
+        String[][] array = new String[][]{{";)"},
+                                            {"7", ":о"},
+                                            {"9", "3", ":)"},
                                             {"1", "13", "5", ":("}};
 
         Array arr = new Array();
