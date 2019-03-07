88bf4f6efadef28ac47e46914340e53e68c01581
Xakep <artshi2009@yandex.ru>
2019-03-07 14:42:32 +0300

    renamed variable "map"

--- src/lesson3/dictionary/Phonedir.java
+++ src/lesson3/dictionary/Phonedir.java
@@ -3,26 +3,26 @@ package lesson3.dictionary;
 import java.util.*;
 
 public class Phonedir implements IMultiMap {
-    private Map<String, HashSet<String>> map;
+    private Map<String, HashSet<String>> hashMap;
 
     public Phonedir() {
-        this.map = new HashMap<>();
+        this.hashMap = new HashMap<>();
     }
 
     @Override
     public void put(String key, String value) {
         HashSet<String> numbers;
-        if (map.containsKey(key)) {
-            numbers = map.get(key);
+        if (hashMap.containsKey(key)) {
+            numbers = hashMap.get(key);
         } else {
             numbers = new HashSet<>();
         }
         numbers.add(value);
-        map.put(key, numbers);
+        hashMap.put(key, numbers);
     }
 
     @Override
     public Set<String> get(String lastName) {
-        return map.get(lastName);
+        return hashMap.get(lastName);
     }
 }
